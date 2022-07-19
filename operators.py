ca05b42c0b07e6dd5ac83ebf430a45ee38b6c7e6
Bhavik Bhuva <bhavikbhuva80@gmail.com>
2022-07-19 19:04:40 +0530

    typecasting in python

---
+++ operators.py
@@ -0,0 +1,32 @@
+a=30
+b=20
+#arithmetic operators
+print("The value of a + b = ",a+b)
+print("The value of a - b = ",a-b)
+print("The value of a * b = ",a*b)
+print("The value of a / b = ",a/b)
+
+#assignmentOperator
+
+a=50
+a+=2 # eqvivalent to a=a+2 we can use a/=2 a*=2 etc
+print("new a is ",a)
+
+
+#comparision Operator
+
+print(4>10)
+
+
+# Logical Operators
+bool1= True
+bool2 = False
+
+print("the value of bool1 and bool2 is",bool1 and bool2)
+print("the value of bool1 or bool2 is",bool1 or bool2)
+print("the value of not bool2 is",not bool2)
+
+
+
+
+
---
+++ typecasting.py
@@ -0,0 +1,8 @@
+
+a = "10"
+print(type(a))
+a=int(a)
+print(type(a))
+print(a*2)
+
+
