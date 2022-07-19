473887c76ed4a804379aa9cb99fcb8f8dad6940d
Bhavik Bhuva <bhavikbhuva80@gmail.com>
2022-07-19 18:40:56 +0530

    updated variables

--- variables.py
+++ variables.py
@@ -1,2 +1,31 @@
+
 a=10
+A=20
 b="Bhavik"
+b1='Bhavik Bhuva'
+b2='''Bhuva
+Bhavik
+Mansukhbhai'''
+c=10.5
+d=True
+e=None
+
+# printing variables
+print(a)
+print(A)
+print(b)
+print(b1)
+print(b2)
+print(c)
+print(d)
+
+#printing types of variables
+print(type(a)) 
+
+print(type(b))
+print(type(b1))
+print(type(b2))
+print(type(c))
+print(type(d))
+print(type(e))
+
