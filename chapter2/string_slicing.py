a0d146db720e5f1e40f46af45291fe8bc2749512
Bhavik Bhuva <bhavikbhuva80@gmail.com>
2022-07-20 15:54:42 +0530

    structure modified

---
+++ chapter2/string_slicing.py
@@ -0,0 +1,23 @@
+from unicodedata import name
+
+
+member1="Bhavik "
+
+member2="Bhuva "
+
+c=member2+member1+"Mansukhbhai"
+print(c)
+
+print(member1+member2) 
+
+print(member1[0]) #display 0th index character
+
+# member1[0] = "d" #does not work
+
+print(member1[0:5])  #returns 0 to 5 characterrs
+
+print(len(member1)) #Returns length
+
+
+
+
