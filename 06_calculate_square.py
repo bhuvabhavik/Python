f127646e9cc876d57c2d762181b4bdf2e3713bce
Bhavik Bhuva <bhavikbhuva80@gmail.com>
2022-07-20 12:18:31 +0530

    solved practice set for chapter 1

---
+++ 06_calculate_square.py
@@ -0,0 +1,2 @@
+num = int(input("Enter any number to find square :- "))
+print("square of ",num,"is ",num*num)
---
+++ add_2_number.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter first  :- "))
+num2 = int(input("Enter second  :- "))
+print("the sum of",num1," and ",num2 ," is ",num1+num2)
+
