ea49a8476cdb57683ee33a6447436e17f804b164
Bhavik Bhuva <bhavikbhuva80@gmail.com>
2022-07-21 09:59:15 +0530

    string slicing and ftr functions

--- chapter2/string_slicing.py
+++ chapter2/string_slicing.py
@@ -1,12 +1,11 @@
-from unicodedata import name
 
-
-member1="Bhavik "
+member1="Bhavik"
 
 member2="Bhuva "
+print("accessing -1 index of Bhavik :",member1[-1])
 
 c=member2+member1+"Mansukhbhai"
-print(c)
+print(c)   
 
 print(member1+member2) 
 
@@ -14,9 +13,17 @@ print(member1[0]) #display 0th index character
 
 # member1[0] = "d" #does not work
 
-print(member1[0:5])  #returns 0 to 5 characterrs
+print(member1[:5])  #returns 0 to 5 characterrs  @member1[:5]) = member1[0:5])
 
 print(len(member1)) #Returns length
+
+
+
+print("Slicing with skip value")
+
+myName = "Bhavika "
+
+print(myName[0:6:1 ])
 
 
 
